8fbf8aa695cde7773552edf62b34315f40359e09
Emeline Padié <emeline.padie@etu.minesparis.psl.eu>
2024-01-16 16:24:48 +0100

    les tests

---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+import tri_bulle.py
+
+## test 
+def tests_tri_bulle():
+    assert Tri_bulle.tri_bulle('') == ''
+    assert Tri_bulle.tri_bulle('aaa') == 'aaa'
+    assert Tri_bulle.tri_bulle('abc') == 'abc'
+    assert Tri_bulle.tri_bulle('cba') == 'abc'
+    assert Tri_bulle.tri_bulle('zbgtj') == 'bgjtz'
+    
+    
